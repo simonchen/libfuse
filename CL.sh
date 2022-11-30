6f0878aa4cd17469f284a09a5f6f3b019f17d691
xychenxy <chenxinyi1978@hotmail.com>
2022-11-30 23:14:42 +0800

    Update CL.sh

--- CL.sh
+++ CL.sh
@@ -15,7 +15,7 @@ ninja
 
 echo "\n================ Release =============================\n"
 mkdir ./$ENV_HOST
-cp ./build/util/fusermount3 ./$ENV_HOST/fusermount
-cp ./build/util/mount.fuse3 ./$ENV_HOST/mount.fuse
-cp ./build/lib/libfuse3.a ./$ENV_HOST/libfuse.a
+cp ./util/fusermount3 ./$ENV_HOST/fusermount
+cp ./util/mount.fuse3 ./$ENV_HOST/mount.fuse
+cp ./lib/libfuse3.a ./$ENV_HOST/libfuse.a
 ls -lt ./$ENV_HOST/
